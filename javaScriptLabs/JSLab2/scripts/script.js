be00e184534a2528fed5e4c592cca9eedd8af937
alexjones <akjones1992@yahoo.com>
2021-02-26 11:29:55 -0600

    created and finished jslab3

---
+++ javaScriptLabs/JSLab2/scripts/script.js
@@ -0,0 +1,50 @@
+//Add your work to this file. 
+
+/* Example
+This is a variable declaration. 
+Also referred to as a variable definition.
+It typically will have a keyword and then the name 
+of the variable. In this case, the variable is named 
+firstVariable. The keyword let indicates to JavaScript
+that we are defining a new variable.*/
+let firstVariable; 
+
+/* Your turn 
+Define a variable named data */
+
+
+/* Example
+ Here we initialize firstVariable
+ In other words we assign it a value for the first
+ time. */
+ firstVariable = 100;
+
+ /* Now we can use our variable. 
+ Let's print it to the console.
+ 
+ Notice that to use our variable we don't need to
+ use the keyword let again. We just use the name of 
+ the variable.*/
+ firstVariable = 'Wings!'
+ let data = '13'
+ console.log(data)
+ console.log(firstVariable);
+const constVariable = "Can't be changed";
+console.log(constVariable);
+
+ /*Your turn
+ Initialize data to 5000*/
+
+ /*Then print your variable to the console.*/
+// constVariable = "Reassignment";
+var myVar;
+myVar = true;
+myVar = false;
+console.log(myVar);
+
+console.log(varVariable);
+console.log(abc);
+
+let abc;
+var varVariable;
+ 

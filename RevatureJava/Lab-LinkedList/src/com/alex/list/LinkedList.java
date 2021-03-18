561bf127bc8d2c859b061c0f8b964674b4d653e0
alexjones <akjones1992@yahoo.com>
2021-03-18 09:22:46 -0500

    added more to labs

--- RevatureJava/Lab-LinkedList/src/com/alex/list/LinkedList.java
+++ RevatureJava/Lab-LinkedList/src/com/alex/list/LinkedList.java
@@ -3,6 +3,38 @@ package com.alex.list;
 public class LinkedList {
 Node head;
     
-    // utility methods
+    public void add(Integer data) {
+    	Node node = new Node(data);
+    	add(node);
+    }
+    
+    public void add(Node node) {
+    	if (head == null) {
+    		head = node;
+    	}else {
+    		Node runner = head;
+    		while(runner.getNext != null) {
+    			runner = runner.getNext();
+    		}
+    		runner.setNext(node);
+    	}
+    }
+    public void printList() {
+    	Node runner = head;
+    	while(runner != null) {
+    		System.out.print(runner.toString()+"==>");
+    		runner = runner.getNext();
+    	}
+    	System.out.print("null");
+    }
+    public Node remove() {
+    	if(head != null) {
+	    	Node last = head;
+	    	head = head.getNext();
+	    	return last;
+    	}else {
+    		return null;
+    	}
+    }
 
 }
---
+++ RevatureJava/Lab-LinkedList/src/com/alex/list/test.java
@@ -0,0 +1,11 @@
+package com.alex.list;
+
+public class test {
+	public static void main(String[] args) {
+		LinkedList example = new LinkedList();
+		example.add(2);;
+		example.add(4);;
+		
+	}
+
+}
